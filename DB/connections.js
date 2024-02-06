e7cd5f0e63960b1e69b2d038f5172cc222e752b6
Mohamed Motea <mrte3a@gmail.com>
2024-02-07 01:15:53 +0200

    update cors

--- DB/connections.js
+++ DB/connections.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const db_connection =  async()=>{
   await mongoose.connect(process.env.URL_CONNECTION)
+  // await mongoose.connect('mongodb+srv://te3a:vipte3a@learnmongodb.reyowfl.mongodb.net/?retryWrites=true&w=majority')
   .then(()=> console.log('DB CONNECTION SUCCESSFULLY'))
   .catch(()=> console.log('DB CONNECTION FAiL'))
 }
--- index.js
+++ index.js
@@ -3,15 +3,17 @@ import express from 'express'
 import db_connection from './DB/connections.js'
 import globalResponse from './src/Middlewares/globalResponse.js'
 import * as router from './src/Modules/index.routers.js'
+import cors from 'cors'
 config({path:'./config/dev.config.env'})
-
 const app = express()
 app.use(express.json())
 const port = process.env.PORT
 
+app.use(cors())
 app.use('/api/v1/auth',router.auth)
 app.use('/api/v1/user',router.user)
 app.use('/api/v1/message',router.message)
+
 app.use(globalResponse)
 db_connection()
 
